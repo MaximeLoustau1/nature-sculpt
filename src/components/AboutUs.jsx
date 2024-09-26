8099f38353b4d1a19a5f3c04f1cef15b8f4da9e4
Maxime Loustau <maximeloustau@Maximes-MacBook-Pro-2.local>
2024-09-26 22:05:27 +0200

    fixed text in about us

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -19,8 +19,7 @@ function AboutUs({ language }) {
                     </>
                 ) : (
                     <>
-                        Notre famille a créé Nature Sculpt{' '}
-                        <span className="highlighted-text">il y a plus de vingt ans</span>. Notre passion pour les plantes et les arbres
+                        Notre famille a créé Nature Sculpt {' '} <span className="highlighted-text">il y a plus de vingt ans</span>. Notre passion pour les plantes et les arbres
                         nous a amenés à partager nos connaissances et compétences avec le public. Notre équipe est composée d'un{' '}
                         <span className="highlighted-text">architecte paysagiste</span>, d'un agronome, d'un{' '}
                         <span className="highlighted-text">expert en irrigation</span>, d'un spécialiste de l'entretien des jardins,
